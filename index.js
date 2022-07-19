4ebb92d19a24cfc24ba893d63cf01d566a8d2ff2
icaro <icaro2rodrigues@gmail.com>
2022-07-19 03:29:04 -0300

    the choices

--- index.js
+++ index.js
@@ -2,77 +2,77 @@
 function Menu(){
     let LeftArrow=document.querySelector(".LeftArrow");
     let RightArrow=document.querySelector(".RightArrow");
-    let Player=document.querySelector(".player");
-    let name=document.querySelector(".name");
+    let Player=document.querySelectorAll(".player")
+    let NamePosition=document.querySelector(".name");
     
+    //as caracteristicas de cada personagem
     let Characters = [
        {
           name:"Ícaro",
           img: "./Imagens/Ícaro.gif",
-          width: "65px"
+          width: "150px"
        },
        {
           name:"Sostenes",
           img: "./Imagens/Sostenes.gif",
-          width: "20px"
+          width: "80px"
        },
        {
           name:"Luiz",
           img: "./Imagens/Luiz.gif",
-          width: "65px"
+          width: "165px"
        },
        {
           name:"Carvalho",
           img: "./Imagens/Carvalho.gif",
-          width: "65px"
+          width: "150px"
        },
        {
           name:"Macello",
           img: "./Imagens/Macello.gif",
-          width: "65px"
+          width: "150px"
        },
        {
-          name:"Joao",
+          name:"João",
           img: "./Imagens/Joao.gif",
-          width: "65px"
+          width: "150px"
        }
     ]
+    //index pra coletar
     let IndexPlayer=0;
     
+
+    //muda o index e chama a função de mudar player
     LeftArrow.addEventListener("click", ()=>{
      
-       
+      
        IndexPlayer--;
        ChangePlayer(IndexPlayer)
     })
-    
+        //muda o index e chama a função de mudar player
     RightArrow.addEventListener("click", ()=>{
-     
-       
+
        IndexPlayer++;
        ChangePlayer(IndexPlayer)
-    
+      
     })
+    //muda o player
     function ChangePlayer(IndexPlayer){
+      
+
+         let  name= Characters[IndexPlayer].name;
+         let  src= Characters[IndexPlayer].img;
+         let  width= Characters[IndexPlayer].width;
+         Player[0].setAttribute("src", `${src}`);
+         Player[0].style.width= `${width}`;
+         NamePosition.innerHTML = `${name}`
+
+    console.log(name);
+    console.log(src)
+    console.log(width)
+   
     
-      console.log(  Characters[IndexPlayer])
-    
-    
-    }
-    
-    
-    
-    
+    }  
     }
     
     Menu()
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    Menu
